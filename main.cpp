cbca0aea3c4ce7e4dd382a84ddcb1f161f5fa553
GrigoriiSolnyshkin <grisolnyshkin04@gmail.com>
2024-01-01 21:16:50 +0200

    without errors

--- main.cpp
+++ main.cpp
@@ -3,7 +3,5 @@
 int main() {
     std::cout << "Hello world!\n";
 
-    if (2 == 2) {
-    }
     return 0;
 }
--- unit-tests/sample_tests.cpp
+++ unit-tests/sample_tests.cpp
@@ -1,6 +1,5 @@
 #include <doctest.h>
 
 TEST_CASE("sample") {
-    int *p = new int[100];
     CHECK_EQ(2 * 2, 4);
 }
