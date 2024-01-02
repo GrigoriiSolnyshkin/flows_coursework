a9a26e0c605809b903a64db625e78d90ce6a05d3
GrigoriiSolnyshkin <grisolnyshkin04@gmail.com>
2024-01-02 04:12:20 +0200

    .clang-tidy and tests fixed

--- unit-tests/link_cut_tests.cpp
+++ unit-tests/link_cut_tests.cpp
@@ -11,18 +11,18 @@ std::mt19937 generator{42};
 TEST_CASE("depths and roots test") {
     link_cut<int64_t> lc(100);
 
-    for (std::size_t i = 0; i < 100; ++i) {
+    for (int i = 0; i < 100; ++i) {
         if (i % 10 == 0) {
             continue;
         }
         lc.link(i, i / 10 * 10);
     }
 
-    for (std::size_t i = 0; i < 100; ++i) {
+    for (int i = 0; i < 100; ++i) {
         CHECK_EQ(lc.link_cut_root(i), i / 10 * 10);
     }
 
-    for (std::size_t i = 0; i < 100; ++i) {
+    for (int i = 0; i < 100; ++i) {
         if (i % 10) {
             CHECK_EQ(lc.link_cut_depth(i), 1);
         } else {
@@ -32,14 +32,14 @@ TEST_CASE("depths and roots test") {
 
     lc.init();
 
-    for (std::size_t i = 0; i < 100; ++i) {
+    for (int i = 0; i < 100; ++i) {
         if (i % 10 == 0) {
             continue;
         }
         lc.link(i, i - 1);
     }
 
-    for (std::size_t i = 0; i < 100; ++i) {
+    for (int i = 0; i < 100; ++i) {
         CHECK_EQ(lc.link_cut_root(i), i / 10 * 10);
         CHECK_EQ(lc.link_cut_depth(i), i % 10);
     }
@@ -57,7 +57,7 @@ TEST_CASE("link cut parents test") {
         CHECK_EQ(lc.link_cut_depth(i), i);
     }
 
-    for (std::size_t i = 5; i < 100; i += 5) {
+    for (int i = 5; i < 100; i += 5) {
         lc.cut(i);
     }
 
@@ -77,37 +77,37 @@ TEST_CASE("link cut parents test") {
 TEST_CASE("binary tree test") {
     link_cut<int64_t> lc(128);
 
-    for (std::size_t i = 2; i < 128; ++i) {
+    for (int i = 2; i < 128; ++i) {
         lc.link(i, i / 2);
     }
 
-    for (std::size_t i = 1; i < 128; ++i) {
+    for (int i = 1; i < 128; ++i) {
         CHECK_EQ(lc.link_cut_root(i), 1);
     }
 
-    for (std::size_t i = 0; i < 7; ++i) {
-        for (std::size_t j = (1 << i); j < (1 << (i + 1)); ++j) {
+    for (int i = 0; i < 7; ++i) {
+        for (int j = (1 << i); j < (1 << (i + 1)); ++j) {
             CHECK_EQ(lc.link_cut_depth(j), i);
         }
     }
 
-    for (std::size_t j = 16; j < 32; ++j) {
+    for (int j = 16; j < 32; ++j) {
         lc.cut(j);
     }
 
-    for (std::size_t i = 4; i < 7; ++i) {
-        for (std::size_t j = (1 << i); j < (1 << (i + 1)); ++j) {
+    for (int i = 4; i < 7; ++i) {
+        for (int j = (1 << i); j < (1 << (i + 1)); ++j) {
             CHECK_EQ(lc.link_cut_depth(j), i - 4);
             CHECK_EQ(lc.link_cut_root(j), j / (1 << (i - 4)));
         }
     }
 
-    for (std::size_t j = 16; j < 32; ++j) {
+    for (int j = 16; j < 32; ++j) {
         lc.link(j, 1);
     }
 
-    for (std::size_t i = 0; i < 7; ++i) {
-        for (std::size_t j = (1 << i); j < (1 << (i + 1)); ++j) {
+    for (int i = 0; i < 7; ++i) {
+        for (int j = (1 << i); j < (1 << (i + 1)); ++j) {
             CHECK_EQ(lc.link_cut_depth(j), i < 4 ? i : i - 3);
             CHECK_EQ(lc.link_cut_root(j), 1);
         }
@@ -117,16 +117,16 @@ TEST_CASE("binary tree test") {
 TEST_CASE("big path test") {
     link_cut<int64_t> lc(1'000'000);
 
-    for (std::size_t i = 1; i < 1'000'000; ++i) {
+    for (int i = 1; i < 1'000'000; ++i) {
         lc.link(i, i - 1);
     }
 
-    for (std::size_t i = 0; i < 1'000'000; ++i) {
+    for (int i = 0; i < 1'000'000; ++i) {
         CHECK_EQ(lc.link_cut_depth(i), i);
         CHECK_EQ(lc.link_cut_root(i), 0);
     }
 
-    for (std::size_t i = 1; i < 1'000'000; ++i) {
+    for (int i = 1; i < 1'000'000; ++i) {
         lc.cut(i);
     }
 }
@@ -137,22 +137,22 @@ TEST_CASE("big path random test") {
     std::iota(order.begin(), order.end(), 1);
 
     std::shuffle(order.begin(), order.end(), generator);
-    for (std::size_t i = 0; i < 999'999; ++i) {
+    for (int i = 0; i < 999'999; ++i) {
         lc.link(order[i], order[i] - 1);
     }
 
     std::shuffle(order.begin(), order.end(), generator);
-    for (std::size_t i = 0; i < 999'999; ++i) {
+    for (int i = 0; i < 999'999; ++i) {
         CHECK_EQ(lc.link_cut_root(order[i]), 0);
     }
 
     std::shuffle(order.begin(), order.end(), generator);
-    for (std::size_t i = 0; i < 999'999; ++i) {
+    for (int i = 0; i < 999'999; ++i) {
         CHECK_EQ(lc.link_cut_depth(order[i]), order[i]);
     }
 
     std::shuffle(order.begin(), order.end(), generator);
-    for (std::size_t i = 0; i < 999'999; ++i) {
+    for (int i = 0; i < 999'999; ++i) {
         lc.cut(order[i]);
     }
 }
