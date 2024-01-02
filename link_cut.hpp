7b70ae9951a27b15d117b552bf021fc43f2512f7
GrigoriiSolnyshkin <grisolnyshkin04@gmail.com>
2024-01-02 03:08:33 +0200

    link cut base

--- link_cut.hpp
+++ link_cut.hpp
@@ -2,6 +2,7 @@
 #define FLOWS_COURSEWORK_LINK_CUT_HPP
 
 #include <vector>
+#include <optional>
 
 namespace flows_coursework {
 
@@ -10,16 +11,19 @@ namespace link_cut {
 template <typename DataType>
 class link_cut {
     using vertex_t = std::size_t;
+    static constexpr vertex_t NO_VERTEX = 0;
     using child_type = bool;
 
     static constexpr child_type LEFT = true;
     static constexpr child_type RIGHT = false;
 
     struct splay_node {
-        vertex_t left = 0;
-        vertex_t right = 0;
-        vertex_t parent = 0;
+        vertex_t left = NO_VERTEX;
+        vertex_t right = NO_VERTEX;
+        vertex_t parent = NO_VERTEX;
         std::size_t subtree_size = 1;
+        vertex_t link_cut_parent = NO_VERTEX;
+        vertex_t extra_parent = NO_VERTEX;
 
         DataType data{};
 
@@ -35,13 +39,21 @@ class link_cut {
         return nodes_m[node].parent;
     }
 
+    [[nodiscard]] vertex_t &get_extra_parent(vertex_t node) {
+        return nodes_m[node].extra_parent;
+    }
+
     template <child_type CHILD_TYPE>
-    vertex_t &get_child(vertex_t node) {
+    [[nodiscard]] vertex_t &get_child(vertex_t node) {
         if constexpr (CHILD_TYPE) {
             return nodes_m[node].left;
         } else {
             return nodes_m[node].right;
         }
+    }
+
+    [[nodiscard]] vertex_t &get_link_cut_parent(vertex_t node) {
+        return nodes_m[node].link_cut_parent;
     }
 
     void update(vertex_t node) {
@@ -67,6 +79,7 @@ class link_cut {
 
         get_parent(node) = parent_of_parent_of_node;
         get_parent(parent_of_node) = node;
+
         get_child<!CHILD_TYPE>(node) = parent_of_node;
         get_child<CHILD_TYPE>(parent_of_node) = child_of_node;
         if (child_of_node) {
@@ -76,11 +89,15 @@ class link_cut {
         update(parent_of_node);
         update(node);
         update(parent_of_parent_of_node);
+
+        get_extra_parent(node) = get_extra_parent(parent_of_node);
     }
 
     template <child_type CHILD_TYPE>
     void splay_step(vertex_t node, vertex_t parent) {
-        if (auto parent_parent = get_parent(); parent_parent == 0) {
+        if (auto parent_parent = get_parent(parent);
+            parent_parent == NO_VERTEX) {
+
             zig<CHILD_TYPE>(node);
         } else if (parent == get_child<CHILD_TYPE>(parent_parent)) {
             zig<CHILD_TYPE>(parent);
@@ -102,6 +119,39 @@ class link_cut {
         }
     }
 
+    void expose_remove_child(vertex_t node) {
+        if (auto child = get_child<RIGHT>(node); child) {
+            get_child<RIGHT>(node) = NO_VERTEX;
+            get_parent(child) = NO_VERTEX;
+
+            update(node);
+
+            get_extra_parent(child) = node;
+        }
+    }
+
+    void expose_add_child(vertex_t node, vertex_t child) {
+        get_child<RIGHT>(node) = child;
+        get_parent(child) = node;
+
+        update(node);
+    }
+
+    void expose(vertex_t node) {
+        splay(node);
+        expose_remove_child(node);
+
+        vertex_t next_node;
+
+        while ((next_node = get_extra_parent(node))) {
+            splay(next_node);
+            expose_remove_child(next_node);
+            expose_add_child(next_node, node);
+
+            node = next_node;
+        }
+    }
+
   public:
     explicit link_cut(std::size_t n_vertices) : nodes_m(n_vertices + 1) {
         nodes_m[0].subtree_size = 0;
@@ -114,6 +164,53 @@ class link_cut {
     void init() {
         nodes_m.assign(nodes_m.size(), splay_node{});
         nodes_m[0].subtree_size = 0;
+    }
+
+    void cut(vertex_t node) {
+        ++node;
+
+        vertex_t link_cut_parent = get_link_cut_parent(node);
+        expose(link_cut_parent);
+        get_link_cut_parent(node) = NO_VERTEX;
+        splay(node);
+        get_extra_parent(node) = NO_VERTEX;
+    }
+
+    [[nodiscard]] vertex_t link_cut_root(vertex_t node) {
+        ++node;
+
+        expose(node);
+        splay(node);
+        vertex_t child;
+        while ((child = get_child<LEFT>(node))) {
+            node = child;
+        }
+        splay(node);
+        return node - 1;
+    }
+
+    [[nodiscard]] std::optional<vertex_t> link_cut_parent(vertex_t node) {
+        vertex_t lc_parent = get_link_cut_parent(++node);
+        return lc_parent ? std::make_optional(lc_parent - 1) : std::nullopt;
+    }
+
+    void link(vertex_t node, vertex_t parent) {
+        ++node;
+        ++parent;
+
+        get_link_cut_parent(node) = parent;
+
+        splay(node);
+        get_extra_parent(node) = parent;
+        expose(node);
+    }
+
+    [[nodiscard]] std::size_t link_cut_depth(vertex_t node) {
+        ++node;
+        expose(node);
+        splay(node);
+
+        return nodes_m[node].subtree_size - 1;
     }
 };
 
